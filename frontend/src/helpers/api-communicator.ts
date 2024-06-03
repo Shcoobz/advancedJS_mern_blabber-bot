fb9b75e864479123910d08ee16016cfda3f66d3e
Shcoobz <a.boeckmann23@gmail.com>
2024-06-03 16:27:11 +0200

    included res status 201

--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 export async function loginUser(email: string, password: string) {
   const res = await axios.post('/user/login', { email, password });
 
-  if (res.status !== 200) {
-    throw new Error('Unable to login!');
+  if (!(res.status === 200 || res.status === 201)) {
+    throw new Error('Unable to login! ' + res.status);
   }
 
   const data = await res.data;

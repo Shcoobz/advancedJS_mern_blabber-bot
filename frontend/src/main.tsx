cf35d87b6ef1ea8ee605f60d49da37feafe7569c
Shcoobz <a.boeckmann23@gmail.com>
2024-06-03 12:48:41 +0200

    fixed typo in authprovider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App.tsx';
+import { AuthProvider } from './context/AuthContext.tsx';
 
 import { createTheme, ThemeProvider } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
@@ -16,10 +18,12 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>{' '}
+    </AuthProvider>
   </React.StrictMode>
 );

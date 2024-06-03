899fc1897d652044ac0c7e421e61e7b3e670f068
Shcoobz <a.boeckmann23@gmail.com>
2024-06-03 12:45:17 +0200

    implemented authcontext with provider

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,46 @@
+import { ReactNode, createContext, useContext, useEffect, useState } from 'react';
+
+type User = {
+  name: string;
+  email: string;
+};
+
+type UserAuth = {
+  isLoggedIn: boolean;
+  user: User | null;
+
+  login: (email: string, password: string) => Promise<void>;
+  signup: (name: string, email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+};
+
+const AuthContext = createContext<UserAuth | null>(null);
+
+export const AuthProvider = ({ children: { children: ReactNode } }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // fetch if users cookies are valid, skip login
+  }, []);
+
+  async function login(email: string, password: string) {}
+
+  async function signup(name: string, email: string, password: string) {}
+
+  async function logout() {}
+
+  const value = {
+    user,
+    isLoggedIn,
+    login,
+    logout,
+    signup,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export function useAuth() {
+  return useContext(AuthContext);
+}

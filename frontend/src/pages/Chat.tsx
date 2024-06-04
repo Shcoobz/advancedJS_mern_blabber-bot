5c52814c6d3e903bc3ca704acd69d10ea5217153
Shcoobz <a.boeckmann23@gmail.com>
2024-06-04 15:23:53 +0200

    implemented more styling for chat page

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,7 +1,53 @@
-import { Box, Avatar, Typography, Button } from '@mui/material';
+import { Box, Avatar, Typography, Button, IconButton } from '@mui/material';
+import { IoMdSend } from 'react-icons/io';
 import { red } from '@mui/material/colors';
 
 import { useAuth } from '../context/AuthContext';
+import ChatItem from '../components/chat/ChatItem';
+
+const staticChatMessages = [
+  {
+    role: 'user',
+    content: 'Hi there!',
+  },
+  {
+    role: 'assistant',
+    content: 'Hello! How can I assist you today?',
+  },
+  {
+    role: 'user',
+    content: 'I need help with a programming problem.',
+  },
+  {
+    role: 'assistant',
+    content: "Sure, I'd be happy to help. What seems to be the issue?",
+  },
+  {
+    role: 'user',
+    content: "I'm getting an error message when I try to compile my code.",
+  },
+  {
+    role: 'assistant',
+    content: 'Can you provide me with the error message?',
+  },
+  {
+    role: 'user',
+    content: "It says 'SyntaxError: Unexpected token <' on line 27.'",
+  },
+  {
+    role: 'assistant',
+    content:
+      'That error usually indicates a problem with HTML tags in your JavaScript file. Check line 27 for any unexpected HTML characters.',
+  },
+  {
+    role: 'user',
+    content: 'Ah, I see the issue now. Thanks for your help!',
+  },
+  {
+    role: 'assistant',
+    content: "You're welcome! If you have any more questions, feel free to ask.",
+  },
+];
 
 function Chat() {
   const auth = useAuth();
@@ -97,8 +143,34 @@ function Chat() {
             overflowY: 'auto',
             scrollBehavior: 'smooth',
           }}>
-          Chats
+          {staticChatMessages.map((chat, index) => (
+            <ChatItem content={chat.content} role={chat.role} key={index} />
+          ))}
         </Box>
+        <div
+          style={{
+            width: '100%',
+            borderRadius: 8,
+            backgroundColor: 'rgb(17,27,39)',
+            display: 'flex',
+            margin: 'auto',
+          }}>
+          <input
+            type='text'
+            style={{
+              width: '100%',
+              backgroundColor: 'transparent',
+              padding: '10px',
+              border: 'none',
+              outline: 'none',
+              color: 'white',
+              fontSize: '20px',
+            }}
+          />
+          <IconButton sx={{ color: 'white', mx: 1 }}>
+            <IoMdSend />
+          </IconButton>
+        </div>
       </Box>
     </Box>
   );

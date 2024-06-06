4c0bc16e9cfec5d4bd9e121d37f12e1cc3b113bb
Shcoobz <a.boeckmann23@gmail.com>
2024-06-06 17:41:41 +0200

    added code recognition in chat and detection for language

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -1,10 +1,48 @@
 import { Box, Avatar, Typography } from '@mui/material';
 import { useAuth } from '../../context/AuthContext';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-function ChatItem({ content, role }: { content: string; role: 'user' | 'assistant' }) {
+function isCodeBlock(str: string) {
+  if (
+    str.includes('=') ||
+    str.includes(';') ||
+    str.includes('[') ||
+    str.includes(']') ||
+    str.includes('{') ||
+    str.includes('}') ||
+    str.includes('#') ||
+    str.includes('//')
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function extractCodeFromString(message: string) {
+  if (message.includes('```')) {
+    const blocks = message.split('```');
+    const codeBlocks = [];
+
+    for (let i = 1; i < blocks.length; i += 2) {
+      const [language, ...code] = blocks[i].trim().split('\n');
+      const codeContent = code.join('\n');
+
+      if (isCodeBlock(codeContent)) {
+        codeBlocks.push({ language: language.trim(), code: codeContent });
+      } else {
+        codeBlocks.push({ language: '', code: `\`\`\`${language}\n${codeContent}` });
+      }
+    }
+
+    return codeBlocks;
+  }
+}
+
+const ChatItem = ({ content, role }: { content: string; role: 'user' | 'assistant' }) => {
+  const messageBlocks = extractCodeFromString(content);
   const auth = useAuth();
-
-  return role === 'assistant' ? (
+  return role == 'assistant' ? (
     <Box
       sx={{
         display: 'flex',
@@ -18,7 +56,23 @@ function ChatItem({ content, role }: { content: string; role: 'user' | 'assistan
         <img src='openai.png' alt='openai' width={'30px'} />
       </Avatar>
       <Box>
-        <Typography sx={{ fontSize: '20px' }}>{content}</Typography>
+        {!messageBlocks && <Typography sx={{ fontSize: '20px' }}>{content}</Typography>}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((codeBlock, index) => [
+            index > 0 && <Box key={`newline-${index}`} sx={{ height: '1rem' }} />,
+            codeBlock.language && (
+              <Box key={`lang-${codeBlock.code}`}>
+                <Typography variant='body2'>{codeBlock.language}</Typography>
+              </Box>
+            ),
+            <SyntaxHighlighter
+              key={codeBlock.code}
+              style={coldarkDark}
+              language={codeBlock.language || 'javascript'}>
+              {codeBlock.code}
+            </SyntaxHighlighter>,
+          ])}
       </Box>
     </Box>
   ) : (
@@ -35,10 +89,26 @@ function ChatItem({ content, role }: { content: string; role: 'user' | 'assistan
         {auth?.user?.name.split(' ')[1][0]}
       </Avatar>
       <Box>
-        <Typography sx={{ fontSize: '20px' }}>{content}</Typography>
+        {!messageBlocks && <Typography sx={{ fontSize: '20px' }}>{content}</Typography>}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((codeBlock, index) => [
+            index > 0 && <Box key={`newline-${index}`} sx={{ height: '1rem' }} />,
+            codeBlock.language && (
+              <Box key={`lang-${codeBlock.code}`}>
+                <Typography variant='body2'>{codeBlock.language}</Typography>
+              </Box>
+            ),
+            <SyntaxHighlighter
+              key={codeBlock.code}
+              style={coldarkDark}
+              language={codeBlock.language || 'javascript'}>
+              {codeBlock.code}
+            </SyntaxHighlighter>,
+          ])}
       </Box>
     </Box>
   );
-}
+};
 
 export default ChatItem;

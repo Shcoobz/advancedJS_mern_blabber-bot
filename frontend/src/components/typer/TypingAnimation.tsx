a91775f5588c294aa40ca1051ee0232d8ed02842
Shcoobz <a.boeckmann23@gmail.com>
2024-06-13 19:56:49 +0200

    changed inline style to css for TypingAnimation

--- frontend/src/components/typer/TypingAnimation.tsx
+++ frontend/src/components/typer/TypingAnimation.tsx
@@ -1,23 +1,21 @@
 import { TypeAnimation } from 'react-type-animation';
+import '../../css/components/typer/TypingAnimation.css';
 
 function TypingAnimation() {
   return (
     <TypeAnimation
       sequence={[
-        'Blabber Bot - Chat with your own AI',
+        'Blabber Bot - AI at your fingertips',
         1000,
-        'Blabber Bot - Built with OpenAI',
+        'Blabber Bot - Experience advanced AI chat',
         2000,
-        'Blabber Bot - Your own customized ChatGPT',
+        'Blabber Bot - Powered by Chat GPT 3.5',
         1500,
+        'Blabber Bot - Build by Shcoobz',
+        2000,
       ]}
       speed={50}
-      style={{
-        fontSize: '60px',
-        color: 'white',
-        display: 'inline-block',
-        textShadow: '1px 1px 20px #000',
-      }}
+      className='typing-animation'
       repeat={Infinity}
     />
   );

af25476fac34e1a4677d49569d36156ef0ad3c0b
Shcoobz <a.boeckmann23@gmail.com>
2024-06-16 14:26:57 +0200

    finished refactoring ChatItem

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -1,67 +1,27 @@
 import React from 'react';
 import { Box, Avatar, Typography } from '@mui/material';
-import { useAuth } from '../../context/useAuth';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import '../../css/components/chat/ChatItem.css';
+
+import { useAuth } from '../../context/useAuth';
 import { MISC } from '../../constants/MISC';
 import { CHAT_AVATAR } from '../../constants/images';
-import { getInitials } from '../../utils/utils';
+import { ASSISTANT_ROLE, USER_ROLE } from '../../constants/constants';
+import {
+  capitalizeFirstLetter,
+  extractCodeFromString,
+  formatAsList,
+  getInitials,
+} from '../../utils/utils';
 
-function isCodeBlock(str: string) {
-  if (
-    str.includes('=') ||
-    str.includes(';') ||
-    str.includes('[') ||
-    str.includes(']') ||
-    str.includes('{') ||
-    str.includes('}') ||
-    str.includes('#') ||
-    str.includes('//')
-  ) {
-    return true;
-  }
-  return false;
+import '../../css/components/chat/ChatItem.css';
+
+interface ChatItemProps {
+  content: string;
+  role: typeof USER_ROLE | typeof ASSISTANT_ROLE;
 }
 
-function extractCodeFromString(message: string) {
-  if (message.includes('```')) {
-    const blocks = message.split('```');
-    const codeBlocks = [];
-
-    for (let i = 1; i < blocks.length; i += 2) {
-      const [language, ...code] = blocks[i].trim().split('\n');
-      const codeContent = code.join('\n');
-
-      if (isCodeBlock(codeContent)) {
-        codeBlocks.push({ language: language.trim(), code: codeContent });
-      } else {
-        codeBlocks.push({ language: '', code: `\`\`\`${language}\n${codeContent}` });
-      }
-    }
-
-    return codeBlocks;
-  }
-}
-
-function formatAsList(message: string) {
-  const regex = /^(\d+\..+|[-*+]\s*.+)$/gm;
-  if (regex.test(message)) {
-    return message.split('\n').map((item, index) => ({
-      type: 'list',
-      content: item.trim(),
-      index,
-    }));
-  }
-  return null;
-}
-
-function capitalizeFirstLetter(string: string) {
-  if (!string) return '';
-  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-}
-
-const ChatItem = ({ content, role }: { content: string; role: 'user' | 'assistant' }) => {
+const ChatItem = ({ content, role }: ChatItemProps) => {
   const messageBlocks = extractCodeFromString(content);
   const listItems = !messageBlocks ? formatAsList(content) : null;
   const auth = useAuth();
@@ -134,7 +94,7 @@ const ChatItem = ({ content, role }: { content: string; role: 'user' | 'assistan
     );
   }
 
-  return role == 'assistant' ? renderAssistantMsg() : renderUserMsg();
+  return role == ASSISTANT_ROLE ? renderAssistantMsg() : renderUserMsg();
 };
 
 export default ChatItem;
--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -6,3 +6,57 @@ export function getInitials(name: string) {
     parts.length === 1 ? parts[0][0] : `${parts[0][0]}${parts[parts.length - 1][0]}`;
   return initials.toUpperCase();
 }
+
+// ChatItem
+function isCodeBlock(str: string) {
+  if (
+    str.includes('=') ||
+    str.includes(';') ||
+    str.includes('[') ||
+    str.includes(']') ||
+    str.includes('{') ||
+    str.includes('}') ||
+    str.includes('#') ||
+    str.includes('//')
+  ) {
+    return true;
+  }
+  return false;
+}
+
+export function extractCodeFromString(message: string) {
+  if (message.includes('```')) {
+    const blocks = message.split('```');
+    const codeBlocks = [];
+
+    for (let i = 1; i < blocks.length; i += 2) {
+      const [language, ...code] = blocks[i].trim().split('\n');
+      const codeContent = code.join('\n');
+
+      if (isCodeBlock(codeContent)) {
+        codeBlocks.push({ language: language.trim(), code: codeContent });
+      } else {
+        codeBlocks.push({ language: '', code: `\`\`\`${language}\n${codeContent}` });
+      }
+    }
+
+    return codeBlocks;
+  }
+}
+
+export function formatAsList(message: string) {
+  const regex = /^(\d+\..+|[-*+]\s*.+)$/gm;
+  if (regex.test(message)) {
+    return message.split('\n').map((item, index) => ({
+      type: 'list',
+      content: item.trim(),
+      index,
+    }));
+  }
+  return null;
+}
+
+export function capitalizeFirstLetter(string: string) {
+  if (!string) return '';
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}

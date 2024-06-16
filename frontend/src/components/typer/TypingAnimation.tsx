88e8fb13a74f08c3301d596a17bc16dc0f036208
Shcoobz <a.boeckmann23@gmail.com>
2024-06-16 14:47:04 +0200

    finished refactoring TypingAnimation

--- frontend/src/components/typer/TypingAnimation.tsx
+++ frontend/src/components/typer/TypingAnimation.tsx
@@ -1,18 +1,30 @@
 import { TypeAnimation } from 'react-type-animation';
+
+import {
+  DELAY_1,
+  DELAY_2,
+  DELAY_3,
+  DELAY_4,
+  SEQUENCE_1,
+  SEQUENCE_2,
+  SEQUENCE_3,
+  SEQUENCE_4,
+} from '../../constants/constants';
+
 import '../../css/components/typer/TypingAnimation.css';
 
 function TypingAnimation() {
   return (
     <TypeAnimation
       sequence={[
-        'Blabber Bot - AI at your fingertips',
-        1000,
-        'Blabber Bot - Experience advanced AI chat',
-        2000,
-        'Blabber Bot - Powered by Chat GPT 3.5',
-        1500,
-        'Blabber Bot - Build by Shcoobz',
-        2000,
+        SEQUENCE_1,
+        DELAY_1,
+        SEQUENCE_2,
+        DELAY_2,
+        SEQUENCE_3,
+        DELAY_3,
+        SEQUENCE_4,
+        DELAY_4,
       ]}
       speed={50}
       className='typing-animation'
--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -3,6 +3,17 @@ export const TECH_DESC = 'MERN';
 export const HYPHEN = ' - ';
 export const PROJ_NAME = 'Blabber Bot';
 
+// Typing Animation
+export const SEQUENCE_1 = `${PROJ_NAME} - AI at your fingertips`;
+export const SEQUENCE_2 = `${PROJ_NAME} - Experience advanced AI chat`;
+export const SEQUENCE_3 = `${PROJ_NAME} - Powered by Chat GPT 3.5`;
+export const SEQUENCE_4 = `${PROJ_NAME} - Built by Shcoobz`;
+
+export const DELAY_1 = 1000;
+export const DELAY_2 = 2000;
+export const DELAY_3 = 1500;
+export const DELAY_4 = 2000;
+
 // Roles
 export const ASSISTANT_ROLE = 'assistant';
 export const USER_ROLE = 'user';

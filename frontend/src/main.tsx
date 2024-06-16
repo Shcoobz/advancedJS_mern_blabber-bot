3d8e01a5103224951e3397c06f1030d6ec4f64e9
Shcoobz <a.boeckmann23@gmail.com>
2024-06-16 12:25:28 +0200

    added css styling for main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,35 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import axios from 'axios';
-import { createTheme, ThemeProvider } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.tsx';
 import { Toaster } from 'react-hot-toast';
 
 import App from './App.tsx';
 
+import { theme } from '../src/constants/theme.ts';
 import './index.css';
-
-// TODO:
-// * implement one name signup -> check if name is one word, or at least two
-// *fix error loading chats after logout
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
-
-const theme = createTheme({
-  typography: {
-    fontFamily: 'Roboto Slab, sans-serif',
-    allVariants: { color: 'whitesmoke' },
-  },
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
         <ThemeProvider theme={theme}>
-          <Toaster position='top-right' />
+          <Toaster />
           <App />
         </ThemeProvider>
       </BrowserRouter>{' '}

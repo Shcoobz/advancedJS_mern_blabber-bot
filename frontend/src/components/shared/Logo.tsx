eef19c384beababc430b44f15a65205846736ebf
Shcoobz <a.boeckmann23@gmail.com>
2024-06-23 15:28:53 +0200

    refactored logo  and added documentation

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -1,21 +1,33 @@
 import { Link } from 'react-router-dom';
 import { Typography } from '@mui/material';
 
-import { NAVIGATION } from '../../constants/constants';
+import { NAVIGATION, INFO } from '../../constants/constants';
 import { NAV_OPENAI_IMG } from '../../constants/assets';
-import { INFO } from '../../constants/constants';
 
 import '../../css/components/shared/Logo.css';
 
-function Logo() {
-  return (
+/**
+ * Logo component displays the company logo along with some descriptive text.
+ * This component is used for branding and navigation purposes.
+ * @returns {JSX.Element} A logo component with navigational and descriptive text.
+ */
+function Logo(): JSX.Element {
+  /**
+   * logoContent holds the JSX for the logo and its accompanying text, providing a clickable image that
+   * navigates to the home page and a description of the technology or project.
+   */
+  const logoContent = (
     <div className='logo-container'>
       <Link to={NAVIGATION.HOME}>{NAV_OPENAI_IMG}</Link>
       <Typography className='logo-text'>
-        <span>{INFO.TECH_DESC}</span> {INFO.HYPHEN} {INFO.PROJ_NAME}
+        <span>{INFO.TECH_DESC}</span>
+        {INFO.HYPHEN}
+        {INFO.PROJ_NAME}
       </Typography>
     </div>
   );
+
+  return logoContent;
 }
 
 export default Logo;

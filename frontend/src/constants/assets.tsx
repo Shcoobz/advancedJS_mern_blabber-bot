5a988556cc41f18da17822381e0c78a791c86361
Shcoobz <a.boeckmann23@gmail.com>
2024-06-23 15:41:51 +0200

    added documentation to assets

--- frontend/src/constants/assets.tsx
+++ frontend/src/constants/assets.tsx
@@ -1,34 +1,72 @@
 import { IoIosLogIn } from 'react-icons/io';
 import '../css/constants/images.css';
 
-// Icons
-export const SUBMIT_ICON = (
+/**
+ * This file contains constant definitions for icons and images used throughout the application.
+ * Each constant is a JSX element styled and configured with specific attributes.
+ */
+
+/**
+ * Icon for the submit button, represented using IoIosLogIn from react-icons.
+ * @constant
+ * @type {JSX.Element}
+ */
+export const SUBMIT_ICON: JSX.Element = (
   <span className='submit-icon'>
     <IoIosLogIn />
   </span>
 );
 
-// Images
-// Nav Bar
-export const NAV_OPENAI_IMG = (
+/**
+ * Image for the navigation bar's OpenAI logo, styled with inversion and specific classes.
+ * @constant
+ * @type {JSX.Element}
+ */
+export const NAV_OPENAI_IMG: JSX.Element = (
   <img src='openai.png' alt='openai' className='logo-inverted logo-image' />
 );
 
-// Home Page
-export const HOME_ROBOT_IMG = <img src='robot.png' alt='robot' className='img' />;
+/**
+ * Image of a robot used on the home page.
+ * @constant
+ * @type {JSX.Element}
+ */
+export const HOME_ROBOT_IMG: JSX.Element = (
+  <img src='robot.png' alt='robot' className='img' />
+);
 
-export const HOME_OPENAI_IMG = (
+/**
+ * Inverted image of the OpenAI logo used on the home page, styled with rotation.
+ * @constant
+ * @type {JSX.Element}
+ */
+export const HOME_OPENAI_IMG: JSX.Element = (
   <img src='openai.png' alt='openai' className='img image-inverted rotate' />
 );
 
-export const HOME_CHAT_IMG = <img src='chat.png' alt='chatbot' className='chat-image' />;
+/**
+ * Image of a chatbot used on the home page.
+ * @constant
+ * @type {JSX.Element}
+ */
+export const HOME_CHAT_IMG: JSX.Element = (
+  <img src='chat.png' alt='chatbot' className='chat-image' />
+);
 
-// Login/Signup Page
-export const GREETING_ROBOT_IMG = (
+/**
+ * Greeting robot image used on the login/signup page.
+ * @constant
+ * @type {JSX.Element}
+ */
+export const GREETING_ROBOT_IMG: JSX.Element = (
   <img src='airobot.png' alt='Robot' className='robot-image' />
 );
 
-// Chat
-export const CHAT_AVATAR = (
+/**
+ * Chat avatar image used in the chat interface.
+ * @constant
+ * @type {JSX.Element}
+ */
+export const CHAT_AVATAR: JSX.Element = (
   <img src='openai.png' alt='openai' className='chat-avatar-image' />
 );

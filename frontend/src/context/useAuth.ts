255ef0bf62713de672476538994fe2658d4456be
Shcoobz <a.boeckmann23@gmail.com>
2024-06-23 15:54:49 +0200

    refactored useAuth and added documentation

--- frontend/src/context/useAuth.ts
+++ frontend/src/context/useAuth.ts
@@ -1,6 +1,18 @@
 import { useContext } from 'react';
 import { AuthContext } from './AuthContext';
 
+/**
+ * Custom hook to access the authentication context.
+ * Utilizes React's useContext to provide the current authentication state and functions.
+ *
+ * @returns {UserAuth | null} The current authentication context value, which includes
+ *                            the user object, authentication status, and authentication functions.
+ */
 export function useAuth() {
-  return useContext(AuthContext);
+  /**
+   * Retrieves the authentication context value using React's useContext hook.
+   */
+  const authContext = useContext(AuthContext);
+
+  return authContext;
 }

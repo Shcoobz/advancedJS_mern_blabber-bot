f5010e33c138a266f030aa066f69ae569007b551
Shcoobz <a.boeckmann23@gmail.com>
2024-06-23 15:33:28 +0200

    refactored typingAnimation and added documentation

--- frontend/src/components/typer/TypingAnimation.tsx
+++ frontend/src/components/typer/TypingAnimation.tsx
@@ -4,7 +4,15 @@ import { SEQUENCE_DETAILS } from '../../constants/constants';
 
 import '../../css/components/typer/TypingAnimation.css';
 
+/**
+ * TypingAnimation component to display a typewriter effect with multiple messages and delays.
+ * Uses the TypeAnimation component from 'react-type-animation' to create an infinite typing effect.
+ */
 function TypingAnimation() {
+  /**
+   * animationSequence defines a list of messages and delays that create a typewriter effect.
+   * This array alternates between messages to type and the delay before removing the text and typing the next message.
+   */
   const animationSequence = [
     SEQUENCE_DETAILS.SEQ_1.message,
     SEQUENCE_DETAILS.SEQ_1.delay,
@@ -16,7 +24,11 @@ function TypingAnimation() {
     SEQUENCE_DETAILS.SEQ_4.delay,
   ];
 
-  return (
+  /**
+   * Defines the TypeAnimation component with configured properties for an infinite typing animation.
+   * This encapsulates all settings including speed, sequence, and styling within a single variable.
+   */
+  const typingAnimation = (
     <TypeAnimation
       sequence={animationSequence}
       speed={50}
@@ -24,6 +36,8 @@ function TypingAnimation() {
       repeat={Infinity}
     />
   );
+
+  return typingAnimation;
 }
 
 export default TypingAnimation;

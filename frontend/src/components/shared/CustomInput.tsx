5d3a719f4709d71ebe608883964b9649c60e72bf
Shcoobz <a.boeckmann23@gmail.com>
2024-06-23 15:26:30 +0200

    refactored customInput and added documentation

--- frontend/src/components/shared/CustomInput.tsx
+++ frontend/src/components/shared/CustomInput.tsx
@@ -1,14 +1,30 @@
 import { TextField } from '@mui/material';
 import '../../css/components/shared/CustomInput.css';
 
+/**
+ * Defines the properties for the CustomInput component.
+ * @property {string} name - Name attribute of the input element.
+ * @property {string} type - Type of the input element, e.g., 'text', 'password', etc.
+ * @property {string} label - Label text for the input element.
+ */
 type Props = {
   name: string;
   type: string;
   label: string;
 };
 
+/**
+ * CustomInput creates a styled text field based on Material-UI's TextField component.
+ * It is designed to be reusable with custom styles applied for different parts of the input.
+ *
+ * @param {Props} props - The properties passed to customize the input component.
+ * @returns {JSX.Element} The customized text field component.
+ */
 function CustomInput(props: Props) {
-  return (
+  /**
+   * textField variable contains the JSX for the TextField component configured with props and custom styles.
+   */
+  const textField = (
     <TextField
       InputLabelProps={{
         className: 'custom-input-label',
@@ -21,6 +37,8 @@ function CustomInput(props: Props) {
       }}
     />
   );
+
+  return textField;
 }
 
 export default CustomInput;

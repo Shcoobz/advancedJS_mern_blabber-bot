8601e5d9de662406f4d74c595c5bea161cb67af3
Shcoobz <a.boeckmann23@gmail.com>
2024-06-23 16:25:47 +0200

    refactored code and added documentation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,22 +6,56 @@ import Footer from '../components/footer/Footer';
 
 import '../css/pages/Home.css';
 
+/**
+ * Home component that renders the homepage of the application.
+ * The homepage includes a typing animation, several images, a project screenshot, and a footer.
+ *
+ * @returns {JSX.Element} The JSX content for the homepage.
+ */
 function Home() {
-  return (
+  /**
+   * JSX content for the typing animation section.
+   * This section includes the TypingAnimation component wrapped in a Box.
+   */
+  const typingAnimation = (
+    <Box>
+      <TypingAnimation />
+    </Box>
+  );
+
+  /**
+   * JSX content for the images section.
+   * This section includes the HOME_ROBOT_IMG and HOME_OPENAI_IMG wrapped in a Box with the class 'img-container'.
+   */
+  const images = (
+    <Box className='img-container'>
+      {HOME_ROBOT_IMG}
+      {HOME_OPENAI_IMG}
+    </Box>
+  );
+
+  /**
+   * JSX content for the project screenshot section.
+   * This section includes the HOME_CHAT_IMG wrapped in a Box with the class 'chat-container'.
+   */
+  const projectScreenshot = <Box className='chat-container'>{HOME_CHAT_IMG}</Box>;
+
+  /**
+   * JSX content for the entire Home component.
+   * This section includes the typing animation, images, project screenshot, and footer, all wrapped in a Box with the class 'container'.
+   */
+  const content = (
     <Box className='container'>
       <Box className='typing-animation-container'>
-        <Box>
-          <TypingAnimation />
-        </Box>
-        <Box className='img-container'>
-          {HOME_ROBOT_IMG}
-          {HOME_OPENAI_IMG}
-        </Box>
-        <Box className='chat-container'>{HOME_CHAT_IMG}</Box>
+        {typingAnimation}
+        {images}
+        {projectScreenshot}
         <Footer />
       </Box>
     </Box>
   );
+
+  return content;
 }
 
 export default Home;

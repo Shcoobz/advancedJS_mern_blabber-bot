02304da078acee2bb8cc5287d8f9cda9b127c440
Shcoobz <a.boeckmann23@gmail.com>
2024-06-24 12:30:39 +0200

    added documentation

--- backend/build/config/openai-config.js
+++ backend/build/config/openai-config.js
@@ -1,4 +1,11 @@
 import { Configuration } from 'openai';
+/**
+ * Configures the OpenAI API with the provided API key and organization ID.
+ * The configuration uses environment variables for the API key and organization ID.
+ *
+ * @function
+ * @returns {Configuration} The configured OpenAI API client.
+ */
 export function configureOpenAI() {
     const config = new Configuration({
         apiKey: process.env.OPEN_AI_API_KEY,

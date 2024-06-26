b902728247ac31e95e8ba96e8ae30c6e0b1e99cf
Shcoobz <a.boeckmann23@gmail.com>
2024-06-26 18:00:09 +0200

    added documentation

--- backend/src/utils/cookie-manager.ts
+++ backend/src/utils/cookie-manager.ts
@@ -1,10 +1,21 @@
 import { COOKIE, COOKIE_OPTIONS } from '../constants/constants.js';
 import { createToken } from './token-manager.js';
 
+/**
+ * Clears a specific cookie from the client browser.
+ *
+ * @param {Response} res - The Express response object used to send the clear cookie command to the client.
+ */
 export function deleteCookie(res) {
   res.clearCookie(COOKIE.NAME, COOKIE_OPTIONS);
 }
 
+/**
+ * Manages user session cookies by refreshing or setting a new cookie.
+ *
+ * @param {Response} res - The Express response object used to manage cookies in the client's browser.
+ * @param {any} user - The user object from which the user ID and email are extracted for token creation.
+ */
 export function handleUserCookie(res, user) {
   deleteCookie(res);
 

62d0edb473d7a1ba190522af827279a714c82c5c
Shcoobz <a.boeckmann23@gmail.com>
2024-06-27 19:34:58 +0200

    merged refactoring to master

--- backend/build/routes/chat-routes.js
+++ backend/build/routes/chat-routes.js
@@ -1,20 +1,10 @@
 import { Router } from 'express';
-import { ROUTE } from '../constants/constants.js';
 import { verifyToken } from '../utils/token-manager.js';
 import { validate, chatCompletionValidator } from '../utils/validators.js';
 import { deleteChats, generateChatCompletion, sendChatsToUser, } from '../controllers/chat-controllers.js';
-/**
- * Router for chat-related endpoints.
- * Utilizes middleware for validation and authentication, and controllers to handle the request logic.
- *
- * Routes:
- * - POST: `/new` - Creates a new chat completion. Requires validation and authentication.
- * - GET: `/all-chats` - Retrieves all chats for the authenticated user.
- * - DELETE: `/delete` - Deletes chats based on provided criteria, requires authentication.
- */
 const chatRoutes = Router();
-chatRoutes.post(ROUTE.CHAT.NEW, validate(chatCompletionValidator), verifyToken, generateChatCompletion);
-chatRoutes.get(ROUTE.CHAT.ALL, verifyToken, sendChatsToUser);
-chatRoutes.delete(ROUTE.CHAT.DELETE, verifyToken, deleteChats);
+chatRoutes.post('/new', validate(chatCompletionValidator), verifyToken, generateChatCompletion);
+chatRoutes.get('/all-chats', verifyToken, sendChatsToUser);
+chatRoutes.delete('/delete', verifyToken, deleteChats);
 export default chatRoutes;
 //# sourceMappingURL=chat-routes.js.map
--- backend/build/routes/user-routes.js
+++ backend/build/routes/user-routes.js
@@ -1,26 +1,13 @@
 import { Router } from 'express';
-import { ROUTE } from '../constants/constants.js';
 import { validate, signupValidator, loginValidator } from '../utils/validators.js';
 import { verifyToken } from '../utils/token-manager.js';
 import { getAllUsers, userSignup, userLogin, verifyUser, userLogout, getUserData, } from '../controllers/user-controllers.js';
-/**
- * Router for user-related endpoints.
- * Handles various user operations such as authentication, data retrieval, and session management.
- *
- * Routes:
- * - GET `ROUTE.USER.HOME`: Retrieves all users. No authentication required.
- * - GET `ROUTE.USER.AUTH`: Verifies user authentication. Requires token verification.
- * - GET `ROUTE.USER.DATA`: Retrieves authenticated user's data. Requires token verification.
- * - GET `ROUTE.USER.LOGOUT`: Logs out the user by ending the session. Requires token verification.
- * - POST `ROUTE.USER.SIGNUP`: Registers a new user. Validates input data before processing.
- * - POST `ROUTE.USER.LOGIN`: Authenticates a user and initiates a session. Validates input data.
- */
 const userRoutes = Router();
-userRoutes.get(ROUTE.USER.HOME, getAllUsers);
-userRoutes.get(ROUTE.USER.AUTH, verifyToken, verifyUser);
-userRoutes.get(ROUTE.USER.DATA, verifyToken, getUserData);
-userRoutes.get(ROUTE.USER.LOGOUT, verifyToken, userLogout);
-userRoutes.post(ROUTE.USER.SIGNUP, validate(signupValidator), userSignup);
-userRoutes.post(ROUTE.USER.LOGIN, validate(loginValidator), userLogin);
+userRoutes.get('/', getAllUsers);
+userRoutes.get('/auth-status', verifyToken, verifyUser);
+userRoutes.get('/get-user-data', verifyToken, getUserData);
+userRoutes.get('/logout', verifyToken, userLogout);
+userRoutes.post('/signup', validate(signupValidator), userSignup);
+userRoutes.post('/login', validate(loginValidator), userLogin);
 export default userRoutes;
 //# sourceMappingURL=user-routes.js.map

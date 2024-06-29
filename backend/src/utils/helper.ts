13208a204ce335afbfa6a697ac23f54e4dfa5906
Shcoobz <a.boeckmann23@gmail.com>
2024-06-29 11:11:40 +0200

    debugging

--- backend/src/utils/helper.ts
+++ backend/src/utils/helper.ts
@@ -1,8 +1,13 @@
 import { SUCCESS } from '../constants/constants.js';
 
 // Function to format server start message with dynamic port
-export function getServerStartMessage(port) {
-  const url = `${process.env.BASE_URL}${port}`;
+// export function getServerStartMessage(port) {
+//   const url = `${process.env.BASE_URL}${port}`;
 
-  return SUCCESS.SERVER.START.replace('{{port}}', port).replace('{{url}}', url);
+//   return SUCCESS.SERVER.START.replace('{{port}}', port).replace('{{url}}', url);
+// }
+
+export function getServerStartMessage(port) {
+  const baseUrl = process.env.BASE_URL;
+  return `Server listening && connected to Database! Visit: ${baseUrl}`;
 }
